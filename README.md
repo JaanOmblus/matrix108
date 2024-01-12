5e34c5c4cb9fdc6c9295c456910254e3ee72583d
kvartiil <138615442+kvartiil@users.noreply.github.com>
2024-01-12 21:14:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# matrix108
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kvartiil/matrix108)
---
+++ src/app/matrixInFerryPier/matrixInFerryPier.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { environment, myNewConstant } from '../../environments/environments';
+
+@Component({
+  selector: 'matrix-ferrypier',
+  templateUrl: './matrixInFerryPier.component.html',
+  styleUrls: ['./matrixInFerryPier.component.css']
+})
+export class FerryPierComponent implements OnInit {
+
+
+
+
+  constructor() { //Siin toob sisse
+    
+    }
+
+    ngOnInit() {  
+      
+  } 
+
+}
